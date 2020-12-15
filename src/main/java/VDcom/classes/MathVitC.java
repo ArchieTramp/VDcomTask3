87db7c24fcf0346a84aafc74d919c1c128519b75
Artur <archiesummerrain@gmail.com>
2020-12-16 01:28:18 +0500

    Исключения по типу данных

--- src/main/java/VDcom/classes/MathVitC.java
+++ src/main/java/VDcom/classes/MathVitC.java
@@ -12,17 +12,22 @@ public class MathVitC implements MathVitCInterface {
 
     @Override
     public void mathVitC() {
-        double vitC;
-        double apple;
-        double lime;
-        double lemon;
+        try {
+            double vitC;
+            double apple;
+            double lime;
+            double lemon;
 
-        System.out.println("How many lemon's?");
-        Scanner scanner = new Scanner(System.in);
-        lemon = scanner.nextDouble();
-        lime = lemon * 1.5;
-        apple = lime * 3 / 1.2;
-        vitC = apple * 0.012;
-        System.out.println(lemon + " lemon = " + vitC + " g vitamin C");
+            System.out.println("How many lemon's?");
+            Scanner scanner = new Scanner(System.in);
+            lemon = scanner.nextDouble();
+            lime = lemon * 1.5;
+            apple = lime * 3 / 1.2;
+            vitC = apple * 0.012;
+            System.out.println(lemon + " lemon = " + vitC + " g vitamin C");
+        } catch (Exception e) {
+            System.out.println("Введен неверный тип данных");
+        }
     }
+
 }
